0e1a4279084113419aa62e7acddc17d5aa19ef0c
Sergey Nelepa <nelepa1995@mail.ru>
2015-08-27 18:37:32 +0300

    added conection and read html

--- parser_habr.py
+++ parser_habr.py
@@ -1,14 +1,20 @@
+import csv
 import urllib.request
 from bs4 import BeautifulSoup
- 
+
+BASE_URL = 'http://habrahabr.ru/top/'
+
 def get_html(url):
+	response = urllib.request.urlopen(url);
+	return response.read();
 
 def parse(html):
-
+	soup = BeautifulSoup(html)
+	print(soup);
 
 def main():
-	print('hello');
+	parse(get_html(BASE_URL));
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 	main();
