5c09543b06ba5ba200f2931e9d284c36953af917
Sergey Nelepa <nelepa1995@mail.ru>
2015-08-27 21:07:42 +0300

    added function for parse html

--- parser_habr.py
+++ parser_habr.py
@@ -10,10 +10,19 @@ def get_html(url):
 
 def parse(html):
 	soup = BeautifulSoup(html)
-	print(soup);
+	html_content = soup.find('div', class_='content_left')
+	html_posts = html_content.findAll('div', class_='post')
+	posts = []
+	for html_post in html_posts:
+		posts.append({
+			'title' : html_post.find('h1', class_='title').a.text,
+			#'hubs' : 
+			'content' : html_post.find('div', class_='content').text
+			})
+	return posts;
 
 def main():
-	parse(get_html(BASE_URL));
+	print(parse(get_html(BASE_URL)));
 
 
 if __name__ == '__main__':
